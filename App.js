2d573134664f336c368b55934c8132685c04f134
Simon Bunker <simon@rendermania.com>
2022-07-18 23:18:18 +1000

    working Three React example

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import { THREE, Renderer } from 'expo-three';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GLView
+      style={{ flex: 1 }}
+      onContextCreate={(gl: ExpoWebGLRenderingContext) => {
+        // Create a WebGLRenderer without a DOM element
+        const renderer = new Renderer({ gl });
+        renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera( 75, gl.drawingBufferWidth / gl.drawingBufferHeight, 0.1, 1000 );
+
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+
+        function animate() {
+          requestAnimationFrame( animate );
+  
+          cube.rotation.x += 0.01;
+          cube.rotation.y += 0.01;
+  
+          renderer.render( scene, camera );
+          gl.endFrameEXP();
+        };
+
+        animate();
+
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
